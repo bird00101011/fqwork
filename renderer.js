e2479e376b2bdc35af9346a9672060c405fa2235
bird00101011 <sc2024228@outlook.com>
2025-04-07 09:59:49 +0800

    Signed-off-by: bird00101011 <sc2024228@outlook.com>

--- renderer.js
+++ renderer.js
@@ -8,12 +8,18 @@ function startWork() {
   fq.style.display = 'flex'
   let rs = ts
 
+  let rm = String(Math.floor(rs / 60)).padStart(2, '0')
+  let rt = String(rs % 60).padStart(2, '0')
+  let rtm = `${rm}:${rt}`
+  fq.innerHTML = rtm;
+
   let t = setInterval(() => {
+    rs--;
+    
     let rm = String(Math.floor(rs / 60)).padStart(2, '0')
     let rt = String(rs % 60).padStart(2, '0')
     let rtm = `${rm}:${rt}`
     fq.innerHTML = rtm;
-    rs--;
 
     if (rs < 0) {
       clearInterval(t);
